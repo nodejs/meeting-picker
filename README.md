3d81c3859618b5a1f85ba9ed65cb7d44e81d2c3d
Rich Trott <rtrott@gmail.com>
2021-12-03 19:38:07 -0800

    fix: update for most recent round of meeting picking

--- README.md
+++ README.md
@@ -2,7 +2,20 @@
 
 ## Usage
 
+Put availability data in `data.tsv` and run: `node meeting-picker.js`
+
+You can also pass an argument for how many meeting slots you want selected:
+`node meeting-picker.js 4`
+
+TODO: Explain this better (like how to create and format data.tsv, and what the
+output looks like and means.)
+
 ### With a Google Spreadsheet URL
+
+This currently doesn't work correctly as it picks former TSC members rather than
+current TSC members and it also only works with one of the sheets rather than
+both sheets (Wednesday and Thursday). But you can still try it, especially if
+you want to fix it.
 
 ```console
 $ GOOGLE_DOC_URL=https://docs.google.com/spreadsheets/d/[SPREADSHEET_ID]/export?format=tsv node meeting-picker.js
--- meeting-picker.js
+++ meeting-picker.js
@@ -8,34 +8,34 @@ const LENGTH = +process.argv[2] || 3
 const NUM_RESULTS = +process.argv[3] || 5
 const MINIMUM_SCORE = +process.argv[4] || 0
 
-const GOOGLE_DOC_URL = process.env.GOOGLE_DOC_URL
-
-if (GOOGLE_DOC_URL) {
-  const request = require('request')
-  request({ uri: GOOGLE_DOC_URL }, (err, response) => {
-    if (err) {
-      throw err
-    }
-    const body = response.body
-    const lines = body.split('\n')
-    const header = lines.findIndex(s => s.startsWith('TSC'))
-    const people = []
-    for (let i = header + 1; i < lines.length; i++) {
-      const line = lines[i]
-      if (line.charAt(0) === '\t') break
-      const data = line.split('\t')
-      people.push({
-        name: data[0],
-        scores: data.slice(1, 25),
-        timezone: data[25]
-      })
-    }
-    analyze(people.map(person => person.scores.join('\t')).join('\n'))
-  })
-} else {
-  const raw = require('fs').readFileSync(path.join(__dirname, '/data.tsv'), 'utf-8')
-  analyze(raw)
-}
+// const GOOGLE_DOC_URL = process.env.GOOGLE_DOC_URL
+//
+// if (GOOGLE_DOC_URL) {
+//   const request = require('request')
+//   request({ uri: GOOGLE_DOC_URL }, (err, response) => {
+//     if (err) {
+//       throw err
+//     }
+//     const body = response.body
+//     const lines = body.split('\n')
+//     const header = lines.findIndex(s => s.startsWith('TSC'))
+//     const people = []
+//     for (let i = header + 1; i < lines.length; i++) {
+//       const line = lines[i]
+//       if (line.charAt(0) === '\t') break
+//       const data = line.split('\t')
+//       people.push({
+//         name: data[0],
+//         scores: data.slice(1, 25),
+//         timezone: data[25]
+//       })
+//     }
+//     analyze(people.map(person => person.scores.join('\t')).join('\n'))
+//   })
+// } else {
+const raw = require('fs').readFileSync(path.join(__dirname, '/data.tsv'), 'utf-8')
+analyze(raw)
+// }
 
 function analyze (raw) {
   function split (string, delim) {
@@ -45,6 +45,10 @@ function analyze (raw) {
   }
 
   const timeScores = split(raw, '\n').map(x => split(x, '\t').map(x => +x))
+  // Should be 24 for one day, 48 for two days, etc.
+  // TODO: Add a check that all entries in timeScores are the same length and
+  // that each score is a number between 0 and 5 inclusive.
+  const scoresLength = timeScores[0].length
 
   // Normalize scores. If someone didn't include any fives, then they will cause
   // the selection of lower-scored times for everyone else so that the scores are
@@ -60,7 +64,7 @@ function analyze (raw) {
   // Ignore times that do not work (score of 0 or 1) for more than 60% of
   // attendees. This avoids the selection of equitably terrible times.
   const indexesToIgnore = []
-  for (let i = 0; i < 24; i++) {
+  for (let i = 0; i < scoresLength; i++) {
     const count = data.filter((individualTimePrefs) => {
       return individualTimePrefs[i] > 1
     }).length
@@ -88,7 +92,7 @@ function analyze (raw) {
 
   const allResults = []
 
-  generateCombinations(0, [])
+  generateCombinations(START, [])
 
   allResults.sort((a, b) => a.standardDeviation - b.standardDeviation)
   allResults.slice(0, NUM_RESULTS).forEach((value) => console.log(value))
@@ -120,7 +124,7 @@ function analyze (raw) {
       return
     }
 
-    if (start < 24) {
+    if (start < scoresLength) {
       let next = start + 1
       while (indexesToIgnore.includes(next)) {
         next = next + 1
