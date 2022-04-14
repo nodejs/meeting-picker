e2e84be2491b0298b72e7f16233a984ab5c22484
Rich Trott <rtrott@gmail.com>
2022-04-13 21:25:49 -0700

    chore: move to ESM

--- test/test.js
+++ test/test.js
@@ -1,19 +1,21 @@
 'use strict'
 
-const assert = require('assert')
-const { spawnSync } = require('child_process')
-const fs = require('fs')
-const path = require('path')
+import assert from 'node:assert'
+import { spawnSync } from 'node:child_process'
+import fs from 'node:fs'
+import path from 'node:path'
+import url from 'node:url'
 
 {
-  const inputFiles = fs.readdirSync(path.join(__dirname, 'input'))
+  const inputFiles = fs.readdirSync(new URL('input', import.meta.url))
 
   inputFiles.forEach((fileName) => {
-    const expected = fs.readFileSync(path.join(__dirname, 'output', `${path.basename(fileName, '.tsv')}.out`))
-    const results = spawnSync(process.argv[0], [path.join(__dirname, '..', 'meeting-picker.js'), path.join('test', 'input', fileName)])
+    const expected = fs.readFileSync(new URL(`output/${path.basename(fileName, '.tsv')}.out`, import.meta.url))
+    const results = spawnSync(process.argv[0], [url.fileURLToPath(new URL('../meeting-picker.js', import.meta.url)), path.join('test', 'input', fileName)])
     if (results.error) {
       throw results.error
     }
+    assert.strictEqual(results.stderr.toString(), '')
     assert.strictEqual(results.stdout.toString(), expected.toString(), `output for ${fileName} is different than expected`)
   })
 }
